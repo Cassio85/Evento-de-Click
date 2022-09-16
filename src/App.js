084af440bbe13032f4abbd5418718de74edad1a7
Cassio Pereira <cassioribeirop@gmail.com>
2022-09-16 19:20:32 -0300

    evento de click com mudança de cor

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 import { useState } from "react";
 
 export default function App() {
-  const [novaCor, setNovaCor] = useState("bg-vermelho");
+  const corDaFonteVermelho = "bg-vermelho";
+  const corDaFonteAzul = "bg-azul";
+
+  const [estiloAtualizado, setEstiloAtualizado] = useState(true);
 
   const alterarCor = () => {
-    if (novaCor === "bg-vermelho") {
-      setNovaCor("bg-azul");
-    } else {
-      setNovaCor("bg-vermelho");
-    }
+    setEstiloAtualizado(!estiloAtualizado);
   };
 
   return (
     <div className="display">
       <div className="container">
         <h2>Altere o estilo do elemento</h2>
-        <div className={novaCor}></div>
-        <p>Clique no botão abaixo para alterar a cor do elemento.</p>
+        <div
+          className={estiloAtualizado ? corDaFonteVermelho : corDaFonteAzul}
+        ></div>
+        <p>Clique no botÃ£o abaixo para alterar a cor do elemento.</p>
         <button onClick={alterarCor}>Clique aqui</button>
       </div>
     </div>
   );
 }
+
